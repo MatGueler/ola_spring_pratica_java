7a4f13edb267608ddb3f566ccc48c00f6cd36ebc
Mateus Gueler <mateus.gueler@gmail.com>
2023-01-17 11:23:51 -0300

    initial:
    Create function to use api spring

---
+++ src/main/java/com/testcamp/testcamp/TestcampApplication.java
@@ -0,0 +1,13 @@
+package com.testcamp.testcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testcamp/testcamp/TestcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcamp.testcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
